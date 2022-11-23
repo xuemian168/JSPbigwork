08f7defbba9c6609ead0b049470846cf3b51ee8e
XueMian <xmysnuke@163.com>
2022-11-23 20:47:03 +0800

    1

---
+++ bigwork2/src/cn/ict/run/model/User.java
@@ -0,0 +1,23 @@
+package cn.ict.run.model;
+
+public class User {
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    String password;
+    String username;
+
+}
